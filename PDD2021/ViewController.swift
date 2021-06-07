24eb8f4efb52fd03fc6ec1adff01c4f53735598a
Evgen-ios <jack38irk@icloud.com>
2021-06-07 15:59:16 +0800

    Initial Commit

---
+++ PDD2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PDD2021
+//
+//  Created by Evgeniy Goncharov on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
